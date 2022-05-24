7facd3c9835055c8783bccc1e582fee5ab1be69c
diego <diego.cortinagarcia@gmail.com>
2022-05-24 09:12:31 +0200

    modified test files

--- src/test/java/com/capgemini/AutoTest.java
+++ src/test/java/com/capgemini/AutoTest.java
@@ -21,34 +21,4 @@ class AutoTest {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	void testGetMarca() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetMarca() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetColor() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetColor() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testGetMotor() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	void testSetMotor() {
-		fail("Not yet implemented");
-	}
-
 }
