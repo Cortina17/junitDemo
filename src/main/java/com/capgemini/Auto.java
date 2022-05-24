d27948f33f9e19cd844bcff3e14cfb1abdbe403e
diego <diego.cortinagarcia@gmail.com>
2022-05-24 09:10:52 +0200

    tests generated

--- src/main/java/com/capgemini/Auto.java
+++ src/main/java/com/capgemini/Auto.java
@@ -2,4 +2,47 @@ package com.capgemini;
 
 public class Auto {
 
+	String marca = "Toyota";
+	String color = "Gris";
+	double motor = 2.0;
+
+	public Auto(String marca, String color, double motor) {
+		this.marca = marca;
+		this.color = color;
+		this.motor = motor;
+
+	}
+	
+	public void imprimirInfo(String marca, String color, double motor) {
+		System.out.println("El auto marca: " + this.marca + " es de color: " + " y tiene una potencia de motor: " + this.motor);
+	}
+	
+	public void subirPotenciaMotor(double potencia) {
+		
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getMotor() {
+		return motor;
+	}
+
+	public void setMotor(double motor) {
+		this.motor = motor;
+	}
+
 }
---
+++ src/test/java/com/capgemini/AutoTest.java
@@ -0,0 +1,54 @@
+package com.capgemini;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AutoTest {
+
+	@Test
+	void testAuto() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testImprimirInfo() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSubirPotenciaMotor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMarca() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMarca() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetColor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetColor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetMotor() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetMotor() {
+		fail("Not yet implemented");
+	}
+
+}
