d786bd0801050a3ba961605872443c3d35f3534b
diego <diego.cortinagarcia@gmail.com>
2022-05-24 09:45:32 +0200

    added more cases

--- src/test/java/com/capgemini/calculadoraTest.java
+++ src/test/java/com/capgemini/calculadoraTest.java
@@ -6,14 +6,37 @@ import org.junit.jupiter.api.Test;
 
 class calculadoraTest {
 
+	Calculadora calc = new Calculadora();
+
 	@Test
 	void testSum() {
-		fail("Not yet implemented");
+		int suma = calc.sum(2, 5);
+		int tot = 7;
+
+		System.out.println("@Test sumar(): " + suma + " = " + tot);
+		assertEquals(suma, tot);
+
+		int suma2 = calc.sum(1, 1);
+		int tot2 = 42;
+
+//		System.out.println("@Test sumar(): " + suma2 + " = " + tot2);
+//		assertEquals(suma2, tot2);
+
 	}
 
 	@Test
 	void testRes() {
-		fail("Not yet implemented");
+		int resta = calc.sum(2, 5);
+		int tot = 7;
+
+		System.out.println("@Test sumar(): " + resta + " = " + tot);
+		assertEquals(resta, tot);
+
+		int resta2 = calc.res(1, 1);
+		int tot2 = 42;
+
+//		System.out.println("@Test sumar(): " + resta2 + " = " + tot2);
+//		assertEquals(resta2, tot2);
 	}
 
 }
